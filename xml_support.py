db8d26df57408b251b709bfa0d40c1d337eb63e0
DanielPalacz <daniel.palacz@gmail.com>
2022-01-29 20:31:36 +0100

    updates iterator implementation

--- xml_support.py
+++ xml_support.py
@@ -7,6 +7,7 @@ class XmlParser:
     def __init__(self, filename: str) -> None:
         self.xml_file: TextIOWrapper = open(filename, mode="r", encoding="utf-8")
         self.line_counter: int = 0
+        self.file_position: int = 0
 
     def __iter__(self):
         return self
@@ -17,15 +18,19 @@ class XmlParser:
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.xml_file.close()
 
-    def __next__(self):
+    def __next__(self) -> str:
         self.line_counter += 1
-        return self.xml_file.readline()
+        text_line: str = self.xml_file.readline()
+        if self.file_position == self.xml_file.tell():
+            raise StopIteration
+        self.file_position = self.xml_file.tell()
+        return text_line
 
 
 if __name__ == "__main__":
-    with XmlParser("Wpisy_Rpm.xml") as xml_file_iterator:
+    with XmlParser("hello.txt") as xml_file_iterator:
         for text in xml_file_iterator:
             print(xml_file_iterator.line_counter)
             sleep(1)
-            print(text)
+            print(repr(text))
             print()
