a5cf886294493fcb8afd1f705f0d80bc96643f2c
DanielPalacz <daniel.palacz@gmail.com>
2022-02-01 20:38:33 +0100

    0102 session - tasks - Kacper coding

--- xml_support.py
+++ xml_support.py
@@ -1,36 +1,41 @@
-from _io import TextIOWrapper
-from time import sleep
+import mmap
 
 
 class XmlParser:
 
     def __init__(self, filename: str) -> None:
-        self.xml_file: TextIOWrapper = open(filename, mode="r", encoding="utf-8")
-        self.line_counter: int = 0
-        self.file_position: int = 0
-
-    def __iter__(self):
-        return self
+        self.xml_file = open(filename, mode="r", encoding="utf-8")
+        self.memory_file = mmap.mmap(self.xml_file.fileno(), 0, access=mmap.ACCESS_READ)
 
     def __enter__(self):
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(self, *args, **kwargs):
+        self.memory_file.close()
         self.xml_file.close()
 
-    def __next__(self) -> str:
-        self.line_counter += 1
-        text_line: str = self.xml_file.readline()
-        if self.file_position == self.xml_file.tell():
-            raise StopIteration
-        self.file_position = self.xml_file.tell()
-        return text_line
+    def __iter__(self):
+        while True:
+            yield self.memory_file[:]
 
+
+
+def parser(xml):
+    return "Slowo slow"
+
+# w generatorach - leniwa ewaulacja -- plik jest wczytywany linia po linii
+# w iteratorze obiekt cały wczytywany na raz (w obiektówce)
+
+
+# open
+# iterator
+# generator
+# generator + mmap
+
+# +
 
 if __name__ == "__main__":
-    with XmlParser("hello.txt") as xml_file_iterator:
+    with XmlParser("Wpisy_Rpm.xml") as xml_file_iterator:
         for text in xml_file_iterator:
-            print(xml_file_iterator.line_counter)
-            sleep(1)
-            print(repr(text))
-            print()
+            print(text)
+
